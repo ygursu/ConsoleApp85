f9fff53fc87e03983cc1cb55e13b3689745b9f03
ygursu <yasemingrs@gmail.com>
2024-02-23 23:57:42 +0300

    Proje dosyası ekle.

---
+++ ConsoleApp85/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace klavydentabanyukseklikgirilenparelelkenaralani
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+      
+
+            int taban, yukseklik, alan;
+
+            Console.WriteLine("Lütfen paralelkenarın tabanını giriniz");
+            taban = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Lütfen paralelkenarın yüksekliğini giriniz");
+            yukseklik = Int32.Parse(Console.ReadLine());
+
+            alan = taban * yukseklik;
+
+            Console.WriteLine("Paralel kenarın alanı:" + alan);
+            Console.ReadLine();
+
+
+        }
+    }
+}
